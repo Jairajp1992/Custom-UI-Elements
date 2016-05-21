16862af60c03047e6be0f9ac5a15a3031ae6d596
Jairajp1992 <jairajp1992@gmail.com>
2016-05-22 02:20:13 +0530

    Custom CheckBoxex Check/Uncheck All
    Adding check all and uncheck all functions. Creating a min folder for js
    and css

--- js/custom.ui.js
+++ js/custom.ui.js
@@ -17,27 +17,35 @@ customUI.createCustomCheckBoxes = function(options){
 		if(!product.parent().hasClass("custom-ui-checkbox")){
 			product.wrap("<div>");
 			product.parent().addClass("custom-ui-checkbox icon-"+options.color+" custom-ui-unchecked");
+			product.parent().bind("click", function(){
+				if($(this).hasClass("custom-ui-unchecked")){
+					$(this).removeClass("custom-ui-unchecked");
+					$(this).addClass("custom-ui-checked");
+					$(this).find(options.selector).attr('checked', true);
+					options.onCheck($(this).find(options.selector).attr("name"), $(this).find(options.selector).val());
+				}
+				else{
+					$(this).addClass("custom-ui-unchecked");
+					$(this).removeClass("custom-ui-checked");
+					$(this).find(options.selector).attr('checked', false);
+					options.onUnCheck($(this).find(options.selector).attr("name"), $(this).find(options.selector).val());
+				}
+				$(this).find(options.selector).trigger("change");
+			});
 		}
 	});
 	
 	$(options.selector).css("width", "0px");
-	$(".custom-ui-checkbox").unbind("click");
-	$(".custom-ui-checkbox").bind("click", function(){
-		if($(this).hasClass("custom-ui-unchecked")){
-			$(this).removeClass("custom-ui-unchecked");
-			$(this).addClass("custom-ui-checked");
-			$(this).find(options.selector).attr('checked', true);
-			options.onCheck($(this).find(options.selector).attr("name"), $(this).find(options.selector).val());
-		}
-		else{
-			$(this).addClass("custom-ui-unchecked");
-			$(this).removeClass("custom-ui-checked");
-			$(this).find(options.selector).attr('checked', false);
-			options.onUnCheck($(this).find(options.selector).attr("name"), $(this).find(options.selector).val());
-		}
-		options.onChange();
-	});
+	
 };
 customUI.getChecked = function(selector){
 	return $(".custom-ui-checked").children(selector);
+};
+customUI.unCheckAll = function(selector){
+	var checks = $(".custom-ui-checked").children(selector);
+	checks.trigger("click");
+};
+customUI.checkAll = function(selector){
+	var checks = $(".custom-ui-unchecked").children(selector);
+	checks.trigger("click");
 };
